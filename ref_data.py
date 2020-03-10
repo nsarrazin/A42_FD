fda922401a2cc32aa3c60153fe058be379789abf
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-03-10 16:32:43 +0100

    changed demo script to match new units

--- ref_data.py
+++ ref_data.py
@@ -27,13 +27,15 @@ with open("data.json", "r") as f:
 for key in dataDict.keys():
     print("[\""+key+"\"] = "+dataDict[key]["name"]+ " - " + dataDict[key]["unit"])
 
+units = [dataDict[key]["unit"] for key in dataDict.keys()]
+print(set(units))
 from scipy.integrate import cumtrapz
 
 plt.figure(dpi=150)
 plt.plot(dataDict["time"]["data"], dataDict["lh_engine_FMF"]["data"], label="Left Engine")
 plt.plot(dataDict["time"]["data"], dataDict["rh_engine_FMF"]["data"], label="Right Engine")
 plt.xlabel("Time [s]")
-plt.ylabel("Fuel mass flow (lbs/hr)")
+plt.ylabel("Fuel mass flow (kg/s)")
 plt.legend()
 plt.show()
 
@@ -42,16 +44,16 @@ plt.figure(dpi=150)
 plt.plot(dataDict["time"]["data"], dataDict["lh_engine_itt"]["data"], label="Left")
 plt.plot(dataDict["time"]["data"], dataDict["rh_engine_itt"]["data"], label="Right")
 plt.xlabel("Time [s]")
-plt.ylabel("Engine internal temperature (deg C)")
+plt.ylabel("Engine internal temperature [K]")
 plt.legend()
 plt.show()
 
-lh_fueltot = cumtrapz(np.array(dataDict["lh_engine_FMF"]["data"])/3600, dataDict["time"]["data"])
+lh_fueltot = cumtrapz(np.array(dataDict["lh_engine_FMF"]["data"]), dataDict["time"]["data"])
 # i think the unit value for recorded fuel use says L but it should be in lbs
 # check with the TA maybe idk
 plt.figure(dpi=150)
-plt.plot(lh_fueltot, label="Integrated fuel use [lbs]")
-plt.plot(np.array(dataDict["lh_engine_FU"]["data"]), label="Recorded fuel use [L???]")
+plt.plot(lh_fueltot, label="Integrated fuel use [kg]")
+# plt.plot(np.array(dataDict["lh_engine_FU"]["data"]), label="Recorded fuel use [L???]")
 plt.legend()
 plt.show()
 
@@ -60,6 +62,11 @@ plt.show()
 # to get it you have to run 
 # conda install -c conda-forge cartopy
 # into the anaconda prompt
+
+lon = np.array(dataDict["Gps_long"]["data"])
+lat = np.array(dataDict["Gps_lat"]["data"])
+alt = np.array(dataDict["Dadc1_alt"]["data"])
+
 has_cartopy=True
 try:
     import cartopy.crs as ccrs
@@ -68,7 +75,6 @@ except ModuleNotFoundError:
     has_cartopy=False
 
 if has_cartopy:
-    alt = dataDict["Dadc1_alt"]["data"]
     plt.figure(dpi=140)
     ax = plt.axes(projection=ccrs.PlateCarree())
     ax.add_feature(cartopy.feature.LAND)
@@ -79,7 +85,7 @@ if has_cartopy:
     ax.add_feature(cartopy.feature.RIVERS)
     ax.set_xlim([4., 6.])
     ax.set_ylim([51., 53.])
-    ax.scatter(dataDict["Gps_long"]["data"],dataDict["Gps_lat"]["data"], c=alt, s=0.05, cmap='coolwarm')
+    ax.scatter(np.degrees(lon), np.degrees(lat), c=alt, s=0.05, cmap='coolwarm')
     plt.show()
 
 
@@ -88,18 +94,20 @@ if has_cartopy:
 
 from helpers import wgs84_to_ecef, TEC
 
-lon = np.radians(np.array(dataDict["Gps_long"]["data"]))
-lat = np.radians(np.array(dataDict["Gps_lat"]["data"]))
-alt = np.array(dataDict["Dadc1_alt"]["data"])
 x,y,z = [], [], []
 for lo,la,al in zip(lon,lat,alt):
     x_ecef = wgs84_to_ecef(np.array([lo, la, al]))
     x_ned = x_ecef @ TEC(lo, la)
-    x.append(x_ecef[0])
-    y.append(x_ecef[1])
-    z.append(x_ecef[2])
+    # x.append(x_ecef[0])
+    # y.append(x_ecef[1])
+    # z.append(x_ecef[2])
+    x.append(lo)
+    y.append(la)
+    z.append(al)
+    
+v = np.abs(np.array(dataDict["lh_engine_itt"]["data"]))
+x,y,z = np.array(x), np.array(y), np.array(z)
 
-v = np.array(dataDict["Dadc1_tas"]["data"])
 has_mayavi = True
 try:
     from mayavi import mlab
@@ -107,5 +115,9 @@ except ModuleNotFoundError:
     has_mayavi=False
 
 if has_mayavi:
-    mlab.show(mlab.plot3d(x,y,z,v, tube_radius=100,colormap='Spectral'))
+    x = np.interp(x, (x.min(), x.max()), (-1, +1))
+    y = np.interp(y, (y.min(), y.max()), (-1, +1))
+    z = np.interp(z, (z.min(), z.max()), (0, +2))
+
+    mlab.show(mlab.plot3d(x,y,z,v, tube_radius=0.005,colormap='plasma'))
     input()
