d7a5eacb32bbaf4c4a546d7ddd7f3086df531bdd
Suyi Wen <suyiwen1@gmail.com>
2020-03-18 14:33:42 +0100

    Create fuelmass.py

---
+++ validation/fuelmass.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import numpy as np
+
+with open('data.json') as f:
+  data = json.load(f)
+  
+FFl=data["lh_engine_FMF"]["data"]
+FFr=data["lh_engine_FMF"]["data"]
+
+
+
