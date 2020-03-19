7f08890e8d6b91a56fa6e9b54ee12314b3e5de64
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-03-19 11:51:08 +0100

    added consts file

---
+++ b42fd/consts.py
@@ -0,0 +1,11 @@
+gamma=1.4   #specific heat ratio
+R=287       #gas constant
+rho0=1.225  #sea level air density
+p0=101325   #sea level pressure
+T0=288.15   #sea level temperature
+g0=9.81     #gravitational acceleration
+lamb=-0.0065       #lapse rate
+
+ft=0.3048  #ft to m
+kt=0.51444 #kts to m/s
+lbshr=0.000125998 #lbs/hr to kg/s
