e86afee891087a0e0f533394d3239a9e236dea3a
Suyi Wen <suyiwen1@gmail.com>
2020-03-21 13:06:40 +0100

    Update Cit_par.py

--- b42fd/data_processing/thrust_input.py
+++ b42fd/data_processing/thrust_input.py
@@ -16,6 +16,7 @@ lamb=-0.0065       #lapse rate
 ft=0.3048  #ft to m
 kt=0.51444 #kts to m/s
 lbshr=0.000125998 #lbs/hr to kg/s
+
 #stationary measurement Cl-Cd
 h=np.array([18000, 17990, 18020, 17990,18000, 18010])*ft
 V=np.array([161,131, 222,200, 182, 114])*kt
@@ -55,30 +56,38 @@ def pressure(hp, gamma,T0,lamb,g0,R,p0):
 def temp_diff(T,T_isa):
     return T-T_isa
 
-#input data for stationary measurements Cl-Cd
-thrust_input=np.zeros((6,5))
-hp=0
-Vc=0
-Tm=0
-
-for i in range(len(h)):
-    Vc=V[i]
-    hp=h[i]
-    Tm=TAT[i]
-    MF1=MFl[i]
-    MF2=MFr[i]
-    p=pressure(hp, gamma,T0,lamb,g0,R,p0)
-    M=Mach(Vc,hp, gamma, rho0,p0, p)
-    T=corrected_temp(Tm,M,gamma)
-    T_isa=Atmosphere(hp).temperature
-    Temp_diff=temp_diff(T,T_isa)
-    thrust_input[i,0]=hp
-    thrust_input[i,1]=M
-    thrust_input[i,2]=Temp_diff
-    thrust_input[i,3]=MF1
-    thrust_input[i,4]=MF1
+def Input(h,V,TAT,MFl,MFr, gamma,T0,lamb,g0,R,p0,rho0):
+    "it returns a matrix of input data for computing thrust"
     
-print(thrust_input)    
+    thrust_input=np.zeros((len(h),5))    
+    hp=0    
+    Vc=0
+    Tm=0
+    for i in range(len(h)):
+        Vc=V[i]
+        hp=h[i]
+        Tm=TAT[i]
+        MF1=MFl[i]
+        MF2=MFr[i]
+        p=pressure(hp, gamma,T0,lamb,g0,R,p0)
+        M=Mach(Vc,hp, gamma, rho0,p0, p)
+        T=corrected_temp(Tm,M,gamma)
+        T_isa=Atmosphere(hp).temperature
+        Temp_diff=temp_diff(T,T_isa)
+        thrust_input[i,0]=hp
+        thrust_input[i,1]=M
+        thrust_input[i,2]=Temp_diff
+        thrust_input[i,3]=MF1
+        thrust_input[i,4]=MF2
+        
+    return thrust_input
+
+"""======================================================================================
+                             FOR FLIGHT DATA 
+======================================================================================"""
+
+#input data for stationary measurements Cl-Cd    
+thrust_input= Input(h,V,TAT,MFl,MFr, gamma,T0,lamb,g0,R,p0,rho0)
     
 #stationary measurements elevator trim curve
 h=np.array([18060, 18360, 18940, 18350, 18090, 17680, 18360])*ft
@@ -88,23 +97,28 @@ MFl=np.array([409,407,404,410,416,420, 480])*lbshr
 MFr=np.array([470, 466, 455, 471, 448, 484, 469])*lbshr
 
 #input thrust data elevator trim curve
-thrust_input1=np.zeros((7,5))
+thrust_input1= Input(h,V,TAT,MFl,MFr, gamma,T0,lamb,g0,R,p0,rho0)
 
-for i in range(len(h)):
-    Vc=V[i]
-    hp=h[i]
-    Tm=TAT[i]
-    MF1=MFl[i]
-    MF2=MFr[i]
-    p=pressure(hp, gamma,T0,lamb,g0,R,p0)
-    M=Mach(Vc,hp, gamma, rho0,p0, p)
-    T=corrected_temp(Tm,M,gamma)
-    T_isa=Atmosphere(hp).temperature
-    Temp_diff=temp_diff(T,T_isa)
-    thrust_input1[i,0]=hp
-    thrust_input1[i,1]=M
-    thrust_input1[i,2]=Temp_diff
-    thrust_input1[i,3]=MF1
-    thrust_input1[i,4]=MF1
-    
-print(thrust_input1) 
+"""======================================================================================
+                             FOR REFERENCE DATA 
+======================================================================================"""
+
+#for stationary measurements Cl-Cd 
+h=np.array([5010, 5020, 5020, 5030, 5020, 5110])*ft
+V=np.array([249,221,192,163,130,118])*kt
+TAT=np.array([12.5, 10.5, 8.8, 7.2,6,5.2])+273.15
+MFl=np.array([798,673,561,463,443,474])*lbshr
+MFr=np.array([813,682,579,484,467,499])*lbshr
+
+
+thrust_input2=Input(h,V,TAT,MFl,MFr, gamma,T0,lamb,g0,R,p0,rho0)
+
+#For elevator trim curve
+h=np.array([6060, 6350, 6550, 6880, 6160, 5810, 5310])*ft
+V=np.array([161, 150, 140, 130, 173, 179,192])*kt
+TAT=np.array([5.5, 4.5, 3.5, 2.5, 5.0, 6.2, 8.2])+273.15
+MFl=np.array([462,458,454,449,465,472, 482])*lbshr
+MFr=np.array([486,482,477,473,489,496,505])*lbshr
+
+
+thrust_input3=Input(h,V,TAT,MFl,MFr, gamma,T0,lamb,g0,R,p0,rho0)
