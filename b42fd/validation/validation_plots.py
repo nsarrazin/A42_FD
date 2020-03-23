f0c44614cbd13fe12e32cfcca840d7e7c08d0af1
Kevin de Lange <k.y.t.y.delange@student.tudelft.nl>
2020-03-23 15:52:49 +0100

    push

--- b42fd/validation/validation_plots.py
+++ b42fd/validation/validation_plots.py
@@ -2,7 +2,7 @@ import json
 import control
 import numpy as np
 import matplotlib.pyplot as plt
-from b42fd.numerical_model.model import A_s_h, B_s_h, C_s_h, D_s_h, A_a_h, B_a_h, C_a_h, D_a_h
+from b42fd.numerical_model.model import A_s_h, B_s_h, C_s_h, D_s_h, A_a_h, B_a_h, C_a_h, D_a_h, A_s_l, B_s_l, C_s_l, D_s_l
 
 with open('input.json', 'r') as f:
     raw = f.read()
@@ -19,7 +19,7 @@ dela = data['dutchroll']['delta_a']
 delr = data['dutchroll']['delta_r']
 
 sys_s = control.ss(A_s_h, B_s_h, C_s_h, D_s_h)
-sys_a = control.ss(A_a_h, B_a_h, C_a_h, D_a_h)
+sys_a = control.ss(A_s_l, B_s_l, C_s_l, D_s_l)
 
 # print(list(zip(dela, delr)))
 
@@ -30,10 +30,16 @@ for i in range(4):
     # plt.plot(t, dele)
     plt.show()
 
-T, yout, bla = control.forced_response(sys_a, ta, np.array(list(zip(dela, delr))).T)
-for i in range(4):
-    plt.plot(T, yout[i])
-    # plt.plot(t, dele)
-    plt.show()
+# T, yout, bla = control.forced_response(sys_a, ts, dele)
+# for i in range(4):
+#     plt.plot(T, yout[i])
+#     # plt.plot(t, dele)
+#     plt.show()
+
+# T, yout, bla = control.forced_response(sys_a, ta, np.array(list(zip(dela, delr))).T)
+# for i in range(4):
+#     plt.plot(T, yout[i])
+#     # plt.plot(t, dele)
+#     plt.show()
 
 
