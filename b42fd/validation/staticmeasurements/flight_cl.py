11f85bece54c02e958030ad0ce5dd4d4e229f7d4
Suyi Wen <suyiwen1@gmail.com>
2020-03-23 23:02:18 +0100

    Update flight_cl.py

--- b42fd/validation/staticmeasurements/flight_cl.py
+++ b42fd/validation/staticmeasurements/flight_cl.py
@@ -36,6 +36,7 @@ for i in range(len(h_m)):
     Tm_K=TAT_K[i]
     p=pressure(hp_m, gamma,T0,lamb,g0,R,p0)
     M=Mach(Vc_ms,hp_m, gamma, rho0,p0, p)
+    print(M)
     T=corrected_temp(Tm_K,M,gamma)
     a=sound_speed(gamma,R,T)
     V_TAS[i]=true_airspeed(M,a)
