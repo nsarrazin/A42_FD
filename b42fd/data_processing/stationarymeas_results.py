cc14430adf2539cf6e85ca61d41ecee546d7937e
Suyi Wen <suyiwen1@gmail.com>
2020-03-24 13:30:16 +0100

    Create stationarymeas_results.py

---
+++ b42fd/data_processing/stationarymeas_results.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 13:25:01 2020
+
+@author: suyiwen
+"""
+
+
+#Flight data
+Cm_de= -1.1941458222011172
+Cm_a= -0.5402088243290768
+
+CL_a=4.371485054942859
+CD0=0.016
+e=0.6
+ 
+ 
+ #reference data
+Cm_de_ref= -1.0024724929977598
+Cm_a_ref= -0.4914080848028234
+ 
+CL_a_ref=4.662367336619402
+CD0_ref=0.016
+e_ref=0.88
